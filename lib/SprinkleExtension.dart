346ba0a8eeef75b8ce8c700c964a57821c58528a
Zaiste <oh@zaiste.net>
2020-07-26 14:05:45 +0200

    re/ext: rename the provider extension to `use`

--- lib/SprinkleExtension.dart
+++ lib/SprinkleExtension.dart
@@ -3,5 +3,5 @@ import 'package:flutter/widgets.dart';
 import 'Provider.dart';
 
 extension SprinkleExtension<T> on BuildContext {
-  T fetch<T>() => Provider.of(this).fetch<T>();
+  T use<T>() => Provider.of(this).fetch<T>();
 }
