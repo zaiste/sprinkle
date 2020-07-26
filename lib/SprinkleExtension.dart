1975a255ce3a084024f83e1350889ab2c8c5508d
Zaiste <oh@zaiste.net>
2020-07-26 20:32:04 +0200

    fe: add `display` navigation extension

--- lib/SprinkleExtension.dart
+++ lib/SprinkleExtension.dart
@@ -1,7 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import 'Provider.dart';
 
 extension SprinkleExtension<T> on BuildContext {
   T use<T>() => Provider.of(this).summon<T>();
+
+  display(Widget widget) async {
+    await Navigator.of(this).push(
+      MaterialPageRoute(builder: (_) => widget),
+    );
+  }
+}
+
 }
