4363629e7fd738edb4bfa4b7eda3be544cbd8228
Zaiste <oh@zaiste.net>
2020-07-27 11:18:15 +0200

    fe: reactivity shortcut for int, string and double

--- lib/SprinkleExtension.dart
+++ lib/SprinkleExtension.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:rxdart/rxdart.dart';
 
 import 'Provider.dart';
 
@@ -11,6 +12,17 @@ extension SprinkleExtension<T> on BuildContext {
       MaterialPageRoute(builder: (_) => widget),
     );
   }
+
 }
 
+extension SprinkleInt on int {
+  BehaviorSubject<int> get reactive => BehaviorSubject<int>.seeded(this);
+}
+
+extension SprinkleString on String {
+  BehaviorSubject<String> get reactive => BehaviorSubject<String>.seeded(this);
+}
+
+extension SprinkleDouble on double {
+  BehaviorSubject<double> get reactive => BehaviorSubject<double>.seeded(this);
 }
