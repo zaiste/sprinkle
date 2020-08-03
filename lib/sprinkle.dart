b7cb2e5dbde770e5075d054ff5a2f8fdf815ddef
Zaiste <oh@zaiste.net>
2020-08-03 23:47:49 +0200

    fe: add Sprinkle top level widget

--- lib/sprinkle.dart
+++ lib/sprinkle.dart
@@ -1,3 +1,23 @@
 library sprinkle;
 
 export 'SprinkleExtension.dart';
+
+import 'package:flutter/material.dart';
+
+import 'Provider.dart';
+import 'Supervisor.dart';
+
+class Sprinkle extends StatelessWidget {
+  final Widget child;
+  final Supervisor supervisor;
+
+  Sprinkle({this.child, this.supervisor});
+
+  Widget build(BuildContext context) {
+    return Provider(
+      data: supervisor,
+      child: child,
+    );
+  }
+}
+
