5f21af57a90a189554d381a779dccb22d3d1263e
digital Idea <info@diot.com.sa>
2020-09-01 14:50:25 +0300

    move reactive extension to Manager class

--- lib/src/manager.dart
+++ lib/src/manager.dart
@@ -6,3 +6,28 @@ typedef UseFunction = T Function<T>();
 abstract class Manager {
   void dispose();
 }
+
+extension SprinkleInt on int {
+  BehaviorSubject<int> get reactive => BehaviorSubject<int>.seeded(this);
+}
+
+extension SprinkleString on String {
+  BehaviorSubject<String> get reactive => BehaviorSubject<String>.seeded(this);
+}
+
+extension SprinkleDouble on double {
+  BehaviorSubject<double> get reactive => BehaviorSubject<double>.seeded(this);
+}
+
+extension SprinkleBool on bool {
+  BehaviorSubject<bool> get reactive => BehaviorSubject<bool>.seeded(this);
+}
+
+extension SprinkleList<T> on List<T> {
+  BehaviorSubject<List<T>> get reactive =>
+      BehaviorSubject<List<T>>.seeded(this);
+}
+
+extension SprinkleObject<T> on T {
+  BehaviorSubject<T> get reactive => BehaviorSubject<T>.seeded(this);
+}
