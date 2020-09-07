2cffae48c87a1c6ea68c666ce3f4e07df6a55ec9
Hamed Rezaee <13097270+hamed-rezaee@users.noreply.github.com>
2020-09-07 18:36:37 +0430

    improve supervisor class.

--- lib/src/manager.dart
+++ lib/src/manager.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'package:rxdart/rxdart.dart';
 
 typedef UseFunction = T Function<T>();
 
--- lib/src/sprinkle_extension.dart
+++ lib/src/sprinkle_extension.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:rxdart/rxdart.dart';
 
 import 'provider.dart';
 
